b416c6500350436442edc653465ec8c0a5a12122
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-05-20 16:23:27 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by valcasara-bryan on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
